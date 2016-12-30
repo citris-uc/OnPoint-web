2fa9f190132e641950cfd7f4d8b11336276f9ee8
Dmitri Skjorshammer <dmitriskj@gmail.com>
2016-12-30 15:14:41 -0800

    Adds JWT. Closes #496.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  #-------------------------------------------------------------------------------------------------
+
+  # This allows us to cleanly handle all possible API errors. If something happens, we simply
+  # raise this error and it takes care of rendering the correct JSON response.
+  class API::V0::Error < StandardError
+    attr :message
+    attr :status_code
+
+    def initialize(error_msg, error_status_code)
+      @message     = error_msg
+      @status_code = error_status_code
+    end
+  end
+
+  #-------------------------------------------------------------------------------------------------
+  rescue_from API::V0::Error, :with => :render_json_with_exception
+
+  # This handles access control for all subclasses -- just need to raise the exception (above) and initialize it.
+  def render_json_with_exception(exception)
+    render :json => { :error => exception.message }, :status => exception.status_code
+  end
+
 end
