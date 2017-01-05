7e1442422eb6a84a02ed00516332d32e6d8951bc
Dmitri Skjorshammer <dmitriskj@gmail.com>
2017-01-05 15:22:39 -0800

    Adds :force action to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
 
   namespace :api, :defaults => { :format => :json } do
     namespace :v0 do
-      resources :cards, :only => [:index]
+      resources :cards, :only => [:index] do
+        delete :force, :on => :collection
+      end
     end
   end
+
+
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/82af180466c5'
+
 end
