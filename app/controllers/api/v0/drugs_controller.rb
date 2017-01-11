4323d98f3be096c302a76aca2e2e57d4fb5e4876
Dmitri Skjorshammer <dmitriskj@gmail.com>
2017-01-11 09:46:08 -0800

    Minor bugs with uniquefying rxcui.

--- app/controllers/api/v0/drugs_controller.rb
+++ app/controllers/api/v0/drugs_controller.rb
@@ -5,9 +5,6 @@ class API::V0::DrugsController < API::V0::BaseController
     matches = Drug.find_by_name(params[:query].downcase.strip)
     rxcuis  = matches.map {|m| m["rxcui"]}
 
-    # For now, to speed up query.
-    rxcuis = rxcuis[0..1]
-
     drugs = []
     rxcuis.each do |rxcui|
       d = Drug.new(rxcui)
