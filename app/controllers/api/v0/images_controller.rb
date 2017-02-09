0020d697a778ddaa39a75917e13c1762e0953bb5
Dmitri Skjorshammer <dmitriskj@gmail.com>
2017-02-08 23:31:42 -0800

    Allow to upload any pill bottle image.

--- app/controllers/api/v0/images_controller.rb
+++ app/controllers/api/v0/images_controller.rb
@@ -3,8 +3,12 @@ class API::V0::ImagesController < API::V0::BaseController
   # GET /api/v0/ocrs/parse
 
   def parse
-    image     = params[:image][:id]
-    file_path = Rails.root.join("app", "assets", "images", "test", image)
+    if params[:image] && params[:image][:id]
+      image     = params[:image][:id]
+      file_path = Rails.root.join("app", "assets", "images", "test", image)
+    else
+      file_path = params[:file].path
+    end
 
     resource   = OcrSpace::Resource.new(apikey: "0ad729224588957")
     result_str = resource.clean_convert file: file_path
