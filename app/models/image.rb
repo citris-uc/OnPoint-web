f3683a95286f7759089332a2e9204bd897c32f11
Dmitri Skjorshammer <dmitriskj@gmail.com>
2017-02-27 14:42:37 -0800

    Attempt to parse base64 image.

--- app/models/image.rb
+++ app/models/image.rb
@@ -55,7 +55,8 @@ class Image
 
     file = File.new(path)
 
-    @data  = OcrSpace::FilePost.post('/parse/image', body: { apikey: "0ad729224588957", language: "eng", isOverlayRequired: false, file: file})
+    # @data  = OcrSpace::FilePost.post('/parse/image', body: { apikey: "0ad729224588957", language: "eng", isOverlayRequired: false, file: file})
+    @data  = OcrSpace::FilePost.post('/parse/image', body: { apikey: "0ad729224588957", language: "eng", isOverlayRequired: false, base64image: base64})
 
     puts "\n\n\n@data: #{@data.inspect}\n\n\n"
 
