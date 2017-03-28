b3f2fea0afc5cd1f6402d314e7b7dbacc8fd12ba
Dmitri Skjorshammer <dmitriskj@gmail.com>
2017-03-28 16:12:55 -0700

    Simplifies by adding to cards the questionnaire reminder.

--- app/workers/card_generator_worker.rb
+++ app/workers/card_generator_worker.rb
@@ -1,6 +1,5 @@
 # encoding: utf-8
 
-# Iterates over all cards, setting missed_at or completed_at
 class CardGeneratorWorker
   include Sidekiq::Worker
   sidekiq_options :retry => true, :backtrace => true
@@ -12,17 +11,11 @@ class CardGeneratorWorker
 
     patients = Patient.all
     patients.each do |uid, data|
-      cards = Cards.new(uid, Time.zone.now.strftime("%F"))
+      cards = Cards.new(uid, Time.zone.now)
       cards.get()
 
-      cards.data.to_a.each do |card_id, card_data|
-        next unless card_data["object_type"] == "questionnaire_reminder"
-
-        # At this point, we don't have a Questionnaire Reminder. Let's create one.
-        card_hash = {}
-        card_hash[:object_type]         = "questionnaire_reminder"
-        cards.add(card_hash)
-      end
+      card_hash = {:object_type => "questionnaire_reminder"}
+      cards.add(card_hash)
     end
   end
 end
